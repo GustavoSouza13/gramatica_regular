e336e56104d91f807bb407f06e6d525bc00d5189
Gustavo Souza <gustavocrsouza@gmail.com>
2022-05-15 18:58:55 -0300

    Comentários reorganizados

--- src/ExpressionGrammar.java
+++ src/ExpressionGrammar.java
@@ -131,62 +131,47 @@ public class ExpressionGrammar {
 
                 if (grammarSentences.stream().anyMatch(grammarSentence -> testSentence(grammarSentence, sentencePart))) {
                     if (grammarSentences.stream().anyMatch(grammarSentence -> grammarSentence.equals(sentencePart)) && (canBeTerminal || i == sentence.length())) {
-                        return i; // Encontrou um terminal para o sentencePart e retorna quantos loops foram precisos (sentencePart.length())
+                        return i; // Encontrou um terminal para o sentencePart e retorna quantos loops foram precisos (sentencePart.length()).
                     } else {
-                        // Filtra as grammarSentences que possuem o sentencePart atual ou apenas variáveis
-                        for (String grammarSentence : grammarSentences.stream().filter(grammarSentence -> testSentence(grammarSentence, sentencePart)).collect(Collectors.toSet())) {
+                        for (String grammarSentence : grammarSentences.stream().filter(grammarSentence -> testSentence(grammarSentence, sentencePart)).collect(Collectors.toSet())) { // Filtra as grammarSentences que possuem o sentencePart atual ou apenas variáveis
                             for (String grammarVariableSentence : grammarSentence.split("")) {
-                                // Valida se o grammarVariableSentence é equivalente a um terminal entre as variáveis,
-                                // por exemplo, XXtY, ou se é epsilon (&). Se for, verificar se ele é igual a sentence buscada, se não,
-                                // não aceita.
-                                if (grammarVariableSentence.matches("[a-z]+") || grammarVariableSentence.equals("&")) {
+                                if (grammarVariableSentence.matches("[a-z]+") || grammarVariableSentence.equals("&")) { // Valida se o grammarVariableSentence é equivalente a um terminal entre as variáveis, por exemplo, XXtY, ou se é epsilon (&). Se for, verificar se ele é igual a sentence buscada, se não, não aceita.
                                     if (grammarVariableSentence.equals(sentence) || grammarVariableSentence.equals(sentencePart)) {
                                         sentence = sentence.substring(i);
                                         quantityFindedPositions += i;
                                     } else {
-                                        // quantityFindedPositions é resetado e vai para a próxima grammerSentence possível.
-                                        quantityFindedPositions = 0;
+                                        quantityFindedPositions = 0; // quantityFindedPositions é resetado e vai para a próxima grammerSentence possível.
                                         break; // Pula para a próxima grammarSentence pq não com a atual aceita.
                                     }
                                 } else {
-                                    // Pega os caminhos possíveis a partir de uma variável, ou seja, sempre terá somente uma entry.
                                     Map<String, Set<String>> waysFromVariable = this.grammar.entrySet().stream()
                                             .filter(entry -> grammarVariableSentence.equals(entry.getKey()))
-                                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)); // Pega os caminhos possíveis a partir de uma variável, ou seja, sempre terá somente uma entry.
 
-                                    boolean isVariable = grammarSentence.replaceAll("[a-z]*", "").length() > 1; /*grammarVariableSentence.matches("[A-Z]+") && grammarVariableSentence.length() > 1*/;
-                                    // Se a grammarSentence for igual a grammarValueVariables (só tiver variáveis),
-                                    // ele validará a sentence atual, caso não, avança na sentence.
-                                    String nextSentence = sentence; // FIXME Pode ser que o sentence.substring(i + 1) de IndexOutOfBoundsException, se der, só colocar sentence.substring(Math.min(i + 1, sentence.length())) deve resolver.
-                                    if (nextSentence.isEmpty()) {
-                                        // Aceita caso o waysFromVariable possuir & (epsilon), caso contrário, não aceita.
-                                        if (!new ArrayList<>(waysFromVariable.values()).stream().findFirst().orElse(new HashSet<>()).contains("&")) {
+                                    if (sentence.isEmpty()) {
+                                        if (!new ArrayList<>(waysFromVariable.values()).stream().findFirst().orElse(new HashSet<>()).contains("&")) { // Aceita caso o waysFromVariable possuir & (epsilon), caso contrário, não aceita.
                                             quantityFindedPositions = 0;
                                         }
                                     } else {
-                                        // Chama o testSentence recursivamente para validar com o waysFromVariable, nextSentence e se pode ser terminal
-                                        int findedPosition = testSentence(waysFromVariable, nextSentence, canBeTerminal || isVariable || nextSentence.length() == 1);
+                                        boolean isVariable = grammarSentence.replaceAll("[a-z]*", "").length() > 1; /*grammarVariableSentence.matches("[A-Z]+") && grammarVariableSentence.length() > 1*/;
+
+                                        int findedPosition = testSentence(waysFromVariable, sentence, canBeTerminal || isVariable || sentence.length() == 1); // Chama o testSentence recursivamente para validar com o waysFromVariable, nextSentence e se pode ser terminal
                                         if (findedPosition == 0) {
-                                            // quantityFindedPositions é resetado e vai para a próxima grammerSentence possível.
-                                            quantityFindedPositions = 0;
+                                            quantityFindedPositions = 0; // É resetado e vai para a próxima grammerSentence possível.
                                             break; // Pula para a próxima grammarSentence pq não com a atual aceita.
                                         } else {
-                                            // Avança a sentence para a próxima parte a ser validada.
-                                            sentence = sentence.substring(findedPosition);
-                                            quantityFindedPositions += findedPosition;
+                                            sentence = sentence.substring(findedPosition); // Avança a sentence para a próxima parte a ser validada.
+                                            quantityFindedPositions += findedPosition; // Soma a quantidade de posições que foram validadas e aceitas na recursividade
                                         }
                                     }
                                 }
                             }
 
-                            // Retorna caso tenha aceitado.
-                            if (quantityFindedPositions == localSentence.length() || quantityFindedPositions > 0 && canBeTerminal) {
+                            if (quantityFindedPositions == localSentence.length() || quantityFindedPositions > 0 && canBeTerminal) { // Retorna caso aceite.
                                 return quantityFindedPositions; // Aceita.
                             } else {
-                                // Volta para a sentence original, quantityFindedPositions é resetado e vai para a próxima
-                                // grammerSentence possível.
-                                sentence = localSentence;
-                                quantityFindedPositions = 0;
+                                sentence = localSentence; // Volta para a sentence original (como estava no início do método atual).
+                                quantityFindedPositions = 0; // É resetado pois não aceita.
                             }
                         }
                     }
@@ -199,8 +184,7 @@ public class ExpressionGrammar {
 
     private boolean testSentence(String grammarValue, String sentence) {
         String grammarValueSentence = grammarValue.replaceAll("[A-Z]*", "");
-        // Valida se o grammarValue é igual a sentence ou se possui somente letras maiúsculas (variáveis)
-        return grammarValueSentence.equals(sentence) || grammarValue.replaceAll("[a-z]*", "").matches("[A-Z]+");
+        return grammarValueSentence.equals(sentence) || grammarValue.replaceAll("[a-z]*", "").matches("[A-Z]+"); // Valida se o grammarValue é igual a sentence ou se possui somente letras maiúsculas (variáveis).
     }
 
     private String generate(String sentence) {
